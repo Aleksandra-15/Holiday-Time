358f46fb3d030b9494b6d03d669d73ddb4c871e4
Aleksandra-15 <saha02247@gmail.com>
2026-02-12 17:40:29 +0200

    finish first project

---
+++ js/booking.js
@@ -0,0 +1,13 @@
+document.getElementById("myBtn1").addEventListener("click", function(){
+  window.location.href = "http://localhost:3000/html/carpathians"
+})
+document.getElementById("myBtn2").addEventListener("click", function(){
+  window.location.href = "http://localhost:3000/html/bacota"
+})
+
+document.getElementById("myBtn3").addEventListener("click", function(){
+  window.location.href = "http://localhost:3000/html/kiyv"
+})
+document.getElementById("myBtn4").addEventListener("click", function(){
+  window.location.href = "http://localhost:3000/html/odesa"
+})
