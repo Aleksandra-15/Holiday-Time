70c4b424e6b68e446b0edb860921c02cc6374561
Aleksandra-15 <saha02247@gmail.com>
2026-02-12 22:29:32 +0200

    section

--- js/booking.js
+++ js/booking.js
@@ -2,12 +2,12 @@ document.getElementById("myBtn1").addEventListener("click", function(){
   window.location.href = "http://localhost:3000/html/carpathians"
 })
 document.getElementById("myBtn2").addEventListener("click", function(){
-  window.location.href = "http://localhost:3000/html/bacota"
+window.location.href = "http://localhost:3000/html/bacota"
 })
 
 document.getElementById("myBtn3").addEventListener("click", function(){
-  window.location.href = "http://localhost:3000/html/kiyv"
+window.location.href = "http://localhost:3000/html/kyiv"
 })
 document.getElementById("myBtn4").addEventListener("click", function(){
-  window.location.href = "http://localhost:3000/html/odesa"
+window.location.href = "http://localhost:3000/html/odesa"
 })
